244db91ab5eadaad943b74e003e4f7b7b8f21e84
carlosbarajas19 <cbarajas@student.neumont.edu>
2022-01-11 19:45:16 -0700

    My MVC Website start stuffz

--- CSC260/CSC260/Controllers/HomeController.cs
+++ CSC260/CSC260/Controllers/HomeController.cs
@@ -27,7 +27,10 @@ namespace CSC260.Controllers
         {
             return View();
         }
-
+        public IActionResult Contact()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
