10d97f88d6aa4d59d62eabaa7ec1f3daa46d8a35
carlosbarajas19 <cbarajas@student.neumont.edu>
2022-01-13 15:09:02 -0700

    styling stuff

--- CSC260/CSC260/Controllers/HomeController.cs
+++ CSC260/CSC260/Controllers/HomeController.cs
@@ -11,6 +11,8 @@ namespace CSC260.Controllers
 {
     public class HomeController : Controller
     {
+        private static int count = 0;
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -29,6 +31,18 @@ namespace CSC260.Controllers
         }
         public IActionResult Contact()
         {
+            return View();
+        }
+        public IActionResult Form()
+        {
+            ViewBag.count = count++;
+            return View();
+        }
+
+        public IActionResult Result(string FirstName)
+        {
+            //ViewBag.FirstName = Request.Form["FirstName"];
+            ViewBag.FirstName = FirstName;
             return View();
         }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
