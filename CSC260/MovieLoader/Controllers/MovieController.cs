8cd6b3cb49cc32e2f6f5ced9c270dd14a31d5bf3
carlosbarajas19 <cbarajas@student.neumont.edu>
2022-01-30 18:58:28 -0700

    model stuff

---
+++ CSC260/MovieLoader/Controllers/MovieController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MovieLoader.Models;
+
+namespace MovieLoader.Controllers
+{
+    public class MovieController : Controller
+    {
+
+        private static List<Movie> MovieList = new List<Movie>()
+        {
+             new Movie("Iron Man", 2008, 4.5f),
+             new Movie("Star Wars", 1977, 5.0f),
+             new Movie("The Truman Show", 1998, 4.3f),
+             new Movie("The Princess Bride", 1987, 4.9f)
+
+        };
+
+        public IActionResult Index()
+        {
+            //return View();
+            return View(MovieList);
+        }
+
+        public IActionResult DisplayMovie()
+        {
+            //Movie m = new Movie("Iron Man", 2008, 5.0f),
+            //return View(m);
+            return View();
+        }
+
+        public IActionResult ReturnMovie(string title)
+        {
+            var mv = MovieList.Find(x => x.Title == title);
+            mv.ReturnMovie();
+            return View("MovieIndex", MovieList);
+        }
+        [HttpPost]
+        public IActionResult AddMovie(Movie movie)
+        {
+            
+            if(ModelState.IsValid)
+            {
+                MovieList.Add(movie);
+                return Redirect("/Movie/MovieIndex");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ CSC260/MovieLoader/Models/Movie.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieLoader.Models
+{
+    public class Movie
+    {
+        [Required (ErrorMessage = "Hello??? As you can see, the Title is required... please don't leave it empty ")] 
+        public string Title { get; set; }
+        [Range (2000, 2022, ErrorMessage = "Year must be between 2000 and 2022")]
+        public int Year { get; set; }
+        public float Rating { get; set; }
+
+        public string LoanedTo { get; set; }
+        
+        public DateTime? LoanedDate { get; set; }
+
+        public Movie()
+        {
+
+        }
+
+        public Movie(string title, int year, float rating)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Rating = rating;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Year} - {Rating} stars";
+        }
+
+        public void ReturnMovie()
+        {
+
+        }
+
+    }
+}
